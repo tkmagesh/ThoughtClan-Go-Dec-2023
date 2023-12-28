050b003889c5673410eacddd96dc946f4248a838
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-28 12:33:43 +0530

    pointer exercise - solution

--- 07-pointers/02-exercise.go
+++ 07-pointers/02-exercise.go
@@ -5,19 +5,19 @@ import "fmt"
 func main() {
 	var no int = 100
 	fmt.Println("Before incrementing, no :", no)
-	increment( /* ? */ )
+	increment(&no)
 	fmt.Println("After incrementing, no :", no)
 
 	n1, n2 := 100, 200
 	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
-	swap( /* ? */ )
+	swap(&n1, &n2)
 	fmt.Printf("Aftter swapping, n1 = %d and n2 = %d\n", n1, n2)
 }
 
-func increment( /* ? */ ) /* NO RETURN RESULT */ {
-	/* ? */
+func increment(x *int) /* NO RETURN RESULT */ {
+	*x++
 }
 
-func swap( /* ? */ ) /* NO RETURN RESULT */ {
-
+func swap(x, y *int) /* NO RETURN RESULT */ {
+	*x, *y = *y, *x
 }
